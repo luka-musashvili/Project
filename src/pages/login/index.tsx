70b181d63cdd74120ecb6d4f11c98a1336dcaa5f
lukamusashvili <57059505+lukamusashvili@users.noreply.github.com>
2024-03-05 18:57:17 +0400

    ant form დავიწყე გასწორებაც, Asterisk სიმბოლოს ვერ ვუქრობ მარტივად, ალბათ cssით გადაწერა დაგვჭირდება

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,18 @@
+import { Button, Form, Input } from "antd";
+
+const onFinish = (values: any) => {
+	console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+	console.log("Failed:", errorInfo);
+};
+
+type FieldType = {
+	username?: string;
+	password?: string;
+};
+
 const LoginPage = () => {
 	return (
 		<div className="flex flex-col h-screen">
@@ -5,8 +20,32 @@ const LoginPage = () => {
 				<img src="/ui/logo.png" />
 			</header>
 			<main className="flex-grow">
-				<h3 className="flex justify-center items-center text-[#7ab800] text-lg mt-[45px] mb-[70px]">იუსტიციის სახლის სერვისების მართვის ერთიანი სისტემა</h3>
-				{/* ფორმა აქ */}
+				<h3 className="flex justify-center items-center text-center text-[#7ab800] text-lg mt-[45px] mb-[70px]">იუსტიციის სახლის სერვისების მართვის ერთიანი სისტემა</h3>
+				<Form
+					name="basic"
+					labelCol={{ span: 8 }}
+					wrapperCol={{ span: 16 }}
+					initialValues={{ remember: true }}
+					onFinish={onFinish}
+					onFinishFailed={onFinishFailed}
+					autoComplete="off"
+					colon={false}
+					className="w-[95%] max-w-[600px] mx-auto text-[#616365] px-[60px] py-[45px] border-solid border-2 border-[#dfe1e3]"
+				>
+					<Form.Item<FieldType> className="text-[#616365]" label="მომხმარებლის სახელი" name="username" rules={[{ required: true, message: "მომხმარებლის სახელი სავალდებულოა" }]}>
+						<Input className="text-[#616365]" />
+					</Form.Item>
+
+					<Form.Item<FieldType> label="პაროლი" name="password" rules={[{ required: true, message: "პაროლი სავალდებულოა" }]}>
+						<Input.Password />
+					</Form.Item>
+
+					<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+						<Button type="primary" htmlType="submit">
+							Submit
+						</Button>
+					</Form.Item>
+				</Form>
 			</main>
 			<footer className="flex justify-center items-center h-[42px] text-[#616365] bg-[#e8eaec] text-[13px]">© მონაცემთა გაცვლის სააგენტო 2016</footer>
 		</div>
