b64b3aebfd48b3ba2d5e47796c9e5f02ca3a25f8
Luka Laptop <you@example.com>
2024-03-07 12:10:46 +0400

    but upd

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,9 +1,13 @@
 import { Button, ButtonProps } from "antd";
 import clsx from "clsx";
 
-const ButtonComponent: React.FC<ButtonProps> = ({ children, ...rest }) => {
-  const { type, htmlType, className } = rest;
-  let combinedClassName = clsx(className); // დამატებითი კლასები სამომავლოდ
+const ButtonComponent: React.FC<ButtonProps> = ({
+  children,
+  className,
+  ...rest
+}) => {
+  const { type, htmlType } = rest;
+  let combinedClassName = clsx(className); // დამატებითი კლასებისთვის
   console.log(combinedClassName);
 
   return (
